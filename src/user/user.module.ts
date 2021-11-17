1f88a01e255ebd143bd34f5ccd376b93c66c8485
Vitaliy <270vitalik270@gmail.com>
2021-11-17 16:59:28 +0200

    Ñadded structure to user entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {UserModel} from "./user.model";
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  imports: [
+    SequelizeModule.forFeature([UserModel])
+  ]
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import {UserModel} from "./user.model";
+import {InjectModel} from "@nestjs/sequelize";
+import {CreateUserDto} from "../dto/create-user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  constructor(@InjectModel(UserModel) private userRepository: typeof UserModel) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto)
+    return user
+  }
+
+  async getAll () {
+    const users = await this.userRepository.findAll()
+    return users
+  }
+}
