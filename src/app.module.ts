3d73d12fc97896a82b246861c09b3d4a1bb09446
Vitaliy <270vitalik270@gmail.com>
2021-11-17 15:14:54 +0200

    db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-@Module({})
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'nest-advanced-backend',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+})
 export class AppModule {}
