40ddc9b39c00ee3e84aea36414514e6bf99d3b19
Vitaliy <270vitalik270@gmail.com>
2021-11-20 21:31:01 +0200

    added giving roles, banning users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,8 @@ import { UserModel } from '../models/user.model';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { Roles } from '../auth/roles-auth.decorator';
 import { RolesGuard } from '../guards/roles.guard';
+import {AddRoleToUserDto} from "../dto/add-role-to-user.dto";
+import {BanUserDto} from "../dto/ban-user.dto";
 
 @ApiTags('Users endpoints')
 @Controller('users')
@@ -29,5 +31,23 @@ export class UserController {
   @Get()
   getAll() {
     return this.userService.getAll();
+  }
+
+  @ApiOperation({ summary: 'Set roles for users' })
+  @ApiResponse({ status: 200, type: [UserModel] })
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RolesGuard) // guard that restrict unauthorized users get info from this endpoint && second guard restrict access to not ADMIN users
+  @Post('/role')
+  addRole(@Body() dto: AddRoleToUserDto) {
+    return this.userService.addRole(dto);
+  }
+
+  @ApiOperation({ summary: 'Ban user' })
+  @ApiResponse({ status: 200, type: [UserModel] })
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RolesGuard) // guard that restrict unauthorized users get info from this endpoint && second guard restrict access to not ADMIN users
+  @Post('/ban')
+  ban(@Body() dto: BanUserDto) {
+    return this.userService.ban(dto);
   }
 }
